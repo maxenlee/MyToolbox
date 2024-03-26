a510980ff08c53163bdd8cc9e0465fb6dee7796d
JacobRaffety <155511665+JacobRaffety@users.noreply.github.com>
2024-03-26 16:11:36 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages 
+
+setup(
+  name = 'Toolbox-', 
+  version = '0.1.0',
+  author = 'Jacob Raffety',
+  author_email = 'jacobraffety@gmail.com',
+  packages = find_packages(),
+  license = 'LICENSE.txt',
+  description = 'This is my data science toolbox for useful code.',
+  long_description = open('README.md').read(),
+  install_requires = [
+    #list of project dependencies here 
+  ],
+)
