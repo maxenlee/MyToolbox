c6044aefdc4341978f8b7861ee0256e4ec82b382
Maxen Niedermier <maxenlee123@gmail.com>
2024-04-02 16:13:04 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
+
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
-    name='RaffetyToolbox', 
+    name='MyToolbox',
     version='0.1.0',
-    author='Jacob Raffety',
-    author_email='jacobraffety@gmail.com',
+    author='Maxen Niedermier',
+    author_email='m.lee@maxime.group',
     packages=find_packages(),
-    url='https://github.com/JacobRaffety/RaffetyToolbox',
+    url='https://github.com/maxenlee/MyToolbox',
     license='LICENSE.txt',  # Make sure to specify the correct license file name
     description='This is my data science toolbox for useful code.',
     long_description=open('README.md').read(),
-    install_requires=[
-        'numpy >= 1.11.1'
-    ],
+    long_description_content_type='text/markdown',  # If your README.md is in markdown
+    install_requires=required,
 )
